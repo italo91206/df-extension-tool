62ec349ded6c1e61ccb62d0ccf33f47bd1ae9ecc
Italo Ferreira <italoferreiradjk14@gmail.com>
2022-10-26 00:16:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+- Hover into the item to check it's scrap value
+- The extension will calculate the total amount of scrap value your current inventory has. Will only count slots which aren't locked
---
+++ scripts/content.js
@@ -0,0 +1,313 @@
+const database = {
+    'firepoker': 50,
+    'baseballbat': 50,
+    'penknife': 50,
+    'chefknife': 50,
+    'amputator': 50,
+    'titaniumblade': 50,
+    'betatomcat': 75,
+    'williamsppk': 75,
+    'gerringhp': 75,
+    'huntingknife': 63,
+    'cricketbat': 100,
+    'golfclub': 100,
+    'fiftyseven': 150,
+    'betarx4': 150,
+    'mancinim1': 150,
+    'fireaxe': 163,
+    'combatknife': 163,
+    'mini41': 275,
+    'skorpion': 275,
+    'uzi': 275,
+    'bowieknife': 450,
+    'ironpipe': 450,
+    'nailbat': 675,
+    'lock19': 850,
+    'koltpython': 850,
+    'sl8': 850,
+    'highlander': 850,
+    'fmp90': 850,
+    'm79': 850,
+    'razor': 950,
+    'sportsarmour': 1050,
+    'crowbar': 1275,
+    'beta8000': 1300,
+    'mp5': 1300,
+    'scalpel': 1650,
+    'ck75': 1850,
+    'm24': 1850,
+    'washington870': 1850,
+    'luger': 2500,
+    'chester1300': 2500,
+    'pp90': 2500,
+    'kris': 2550,
+    'shove': 2550,
+    'duskkri': 2550,
+    'xduskkri': 2550,
+    'dawnkri': 2550,
+    'bloodymachet': 2550,
+    'dilmarps': 2550,
+    'lock17': 3250,
+    'lock25': 3250,
+    'webster1942': 3250,
+    'm1garand': 3250,
+    'rebellion': 3250,
+    'washington1100': 3250,
+    'grammm11': 3250,
+    'm16': 3250,
+    'fmfnc': 3250,
+    'sa80': 3250,
+    'mesaacr': 3250,
+    'hk69': 3250,
+    'zylonvest': 3450,
+    'kevlarvest': 3450,
+    'ck99': 4100,
+    'sega20': 4100,
+    'wall500': 4100,
+    'stellaug': 4100,
+    'sicapollo': 5050,
+    'chesterfield': 5050,
+    'm21': 5050,
+    'glacialwarfare': 5050,
+    'spsa12': 5050,
+    'ump': 5050,
+    'trenchknife': 5450,
+    'ronanpro': 5450,
+    'usp': 6100,
+    'woodaxe': 7400,
+    'machete': 7400,
+    'kukri': 9650,
+    'sabre': 9650,
+    'steelms800': 9650,
+    'koltanaconda': 10850,
+    'msg9': 10850,
+    'mannberg500': 10850,
+    'gm94': 10850,
+    'flakjacket': 11050,
+    'sn42': 11050,
+    'sledgehammer': 12200,
+    'ck97b': 14750,
+    'chicagotypewriter': 14750,
+    'xm8': 14750,
+    'fmfal': 14750,
+    'sic550': 16925,
+    'hawkop96': 19250,
+    'sweeper': 19250,
+    'ak47': 19250,
+    'rg6': 19250,
+    'meshslx': 19450,
+    'meshrx2': 19450,
+    'reactiveslx': 19450,
+    'reactivext800': 19450,
+    'battleaxe': 20050,
+    'katana': 20050,
+    'doomcane': 20050,
+    'duskrazor': 20050,
+    'grinder': 20050,
+    'wakizashi': 22100,
+    'desertfox': 24350,
+    'm82a2': 24350,
+    'dragonsvd': 24350,
+    'k50m': 24350,
+    'fmmitrail': 24350,
+    'falconmm1': 24350,
+    'nodachi': 24250,
+    'corpsecrusher': 24250,
+    'steel090': 24250,
+    'corpsegrinder': 24250,
+    'dusksaw': 24250,
+    'sw500': 27125,
+    'spiker': 28850,
+    'sharktail': 28850,
+    'gutsplitter': 28850,
+    'xduskrazor': 28850,
+    'santasslayer': 28850,
+    'xhammer,': 28850,
+    'dawnblade': 28850,
+    'ripsawg12': 28850,
+    'buzzbladern8': 28850,
+    'xdusksaw': 28850,
+    'grinderx3': 28850,
+    'dawnsaw': 28850,
+    'haresplitter': 28850,
+    'alphabull': 40050,
+    'duskenforcer': 40050,
+    'vssvintorez': 40050,
+    'corpseblaster': 40050,
+    'usan12': 40050,
+    'm4': 40050,
+    'fmmag': 40050,
+    'm60': 40050,
+    'gau19': 40050,
+    'xm2': 40050,
+    'exterminatormesh': 40250,
+    'exterminatormeshgt': 40250,
+    'duskmesh': 40250,
+    'exterminatorreactive': 40250,
+    'exterminatorreactivext': 40250,
+    'duskreactive': 40250,
+    'corpsecarapace': 40250,
+    'crissvictor': 44150,
+    'corpseripper': 44150,
+    '475magnum': 48500,
+    'corpseshooter': 48500,
+    '577rex': 48500,
+    'corpsepiercer': 48500,
+    'duskcarbine': 48500,
+    'aa12': 48500,
+    'duskstriker': 48500,
+    'dusksmg': 48500,
+    'corpsedestroyer': 48500,
+    'duskmag': 48500,
+    'vulcan': 48500,
+    'paw20': 48500,
+    'corpseerruptor': 48500,
+    'dusklauncher': 48500,
+    'dualblade': 50000,
+    'goretooth44g': 50000,
+    'greyhawk55': 57650,
+    'vlockxd11': 57650,
+    'doubleshotaax': 57650,
+    'xduskenforcer': 57650,
+    'desertrat': 57650,
+    'xpython': 57650,
+    'chainrevolver': 57650,
+    'dawnenforcer': 57650,
+    'ironsight33f': 57650,
+    'marksmang8': 57650,
+    'worgcarbine': 57650,
+    'xduskcarbine': 57650,
+    'longshotpp10': 57650,
+    'xgarand': 57650,
+    'dawncarbine': 57650,
+    'barnellrf31crossbow': 57650,
+    'painshot10': 57650,
+    'biforcec7': 57650,
+    'acebarrelgz3': 57650,
+    'xduskstriker': 57650,
+    'buckblast99a': 57650,
+    'xmannberg': 57650,
+    'silverslugster': 57650,
+    'dawnstriker': 57650,
+    'chimneysweeper': 57650,
+    'uziel14mm': 57650,
+    'handshocka8': 57650,
+    'streetdog99': 57650,
+    'xdusksmg': 57650,
+    'ninecutterx': 57650,
+    'xmp5': 57650,
+    'santaslittlehelper': 57650,
+    'dawnsmg': 57650,
+    'hammerhead47': 57650,
+    'xak47': 57650,
+    'xduskmag': 57650,
+    'hareraiser': 57650,
+    'dawnmag': 57650,
+    'xlgunner8': 57650,
+    'scar9000': 57650,
+    'wraithcannon': 57650,
+    'immolatorad': 57650,
+    'bishopce9': 57650,
+    'junker6': 57650,
+    'eggsplodermm1': 57650,
+    'xdusklauncher': 57650,
+    'boomerpx': 57650,
+    'xm79': 57650,
+    'dawnlauncher': 57650,
+    'nomadmesh': 128250,
+    'scavengermesh': 128250,
+    'shinobumesh': 128250,
+    'xduskmesh': 128250,
+    'hazardmesh': 128250,
+    'xterminatormesh': 128250,
+    'bioreactive': 128250,
+    'steelcasereactive': 128250,
+    'tatakaureactive': 128250,
+    'xduskreactive': 128250,
+    'scarabreactive': 128250,
+    'xterminatorreactive': 128250,
+    'hazardreactive': 128250,
+    'vengeanceguard': 128250,
+}
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    currencyDisplay: 'symbol',
+    useGrouping: true
+})
+
+const inventory = document.querySelector("table#inventory")
+const inventory_slots = inventory.querySelectorAll("td")
+
+function addPriceTag(nome) {
+    const info_box = document.querySelector("#infoBox")
+    const already = info_box.querySelector("p#extension-item-value")
+    let sanitized_name = nome.split('_')
+    sanitized_name = sanitized_name[0]
+
+    if (already == null && database[sanitized_name]) {
+       
+        let value = database[sanitized_name]
+
+        if (nome.includes('stats'))
+            value *= 2;
+
+        const el = document.createElement("p")
+        el.id = "extension-item-value"
+        el.classList.add("itemData")
+        el.innerText = `Scrap value ${formatter.format(value)}`
+        info_box.appendChild(el)
+    }
+}
+
+function getScrapValue(name){
+    let sanitized_name = name.split('_')
+    sanitized_name = sanitized_name[0]
+
+    let value = database[sanitized_name] || 0
+    if(name.includes('stats'))
+        value *= 2;
+    
+    return value
+}
+
+let total_scrap_value = 0
+
+function addHoverEvent(){
+    inventory_slots.forEach((item) => {
+        let is_locked = item.classList.contains('locked')
+    
+        if (item.hasChildNodes() && !is_locked) {
+            let name = item.children[0].attributes['data-type'].value
+            total_scrap_value += getScrapValue(name)
+    
+            // console.log({
+            //     is_locked: is_locked, 
+            //     class_list: item.classList,
+            //     item: item, 
+            //     item_child: item.children[0]
+            // })
+    
+            item.addEventListener("mousemove", function () {
+                addPriceTag(name)
+            })
+        }
+    });
+}
+
+function addScrapWorthText(){
+    const els = document.querySelectorAll('#inventoryholder > .opElem')
+    els.forEach((e) =>{ 
+        if(e.innerText == 'Backpack')
+            e.innerText += ` - scrap worth ${formatter.format(total_scrap_value)}`
+    })
+}
+
+setTimeout(function(){
+    addHoverEvent();
+    addScrapWorthText();
+}, 1000)
+
+
